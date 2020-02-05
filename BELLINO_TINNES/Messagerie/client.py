54ac05bf92829e5c4ce9d03c1ccbfb96e542a016
Matthieu42 <matthieu.tinnes@gmail.com>
2020-02-05 15:15:20 +0100

    Add aes256

--- BELLINO_TINNES/Messagerie/client.py
+++ BELLINO_TINNES/Messagerie/client.py
@@ -2,35 +2,68 @@
 # coding: utf-8
 
 import socket
+import msgpack
+from Crypto import Random
+from Crypto.Cipher import AES
 
 class Client():    
 
-    def __init__(self, pseudo):
+    def __init__(self, pseudo, key):
         self.pseudo = pseudo
+        self.key = key
 
     def connect(self, hote, port):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((hote, port))
-        print("Connected")
-    
+        try:
+            self.sock.connect((hote, port))
+            print("Connected")
+        except:
+            print("Error : Can't connect to server")
+
+    def init(self):
+        self.sock.send(self.pseudo.encode())
+
     def sendMessage(self,message):
+        self.cipher = AES.new(self.key.encode(), AES.MODE_GCM,self.nounce)
+        ciphertext, tag = self.cipher.encrypt_and_digest(message.encode())
+        self.sock.send(ciphertext)
+    
+    def sendUnsecureMessage(self,message):
         self.sock.send(message.encode())
     
     def disconnect(self):
         self.sock.close()
         print("Disconnected")
+    def setNounce(self,nounce):
+        self.nounce = nounce
 
 
-client = Client("Matthieu")
+if __name__ == "__main__":
+    client = Client("admin","8ff8aec80ea8f2a5ee5e1b6607226399")
 
-client.connect("159.31.61.211",1234)
-
-while True:
+    client.connect("159.31.61.211",1234)
+    client.init()
+    # Demande de mot de passe
+    data = client.sock.recv(1024)
+    print(data.decode() + '\n')
     message = input()
-    client.sendMessage(message)
+    client.sendUnsecureMessage(message)
+    # Receive the nounce
+    data = client.sock.recv(1024)
+    print("Message recu : " + data.decode() + '\n')
 
+    data = client.sock.recv(1024)
+    print(data)
+    client.setNounce(data)
 
-client.disconnect()
+    while True:
+        message = input()
+        client.sendMessage(message)
+        data = client.sock.recv(1024)
+        print("Message recu : " + data.decode() + '\n')
+        
+
+    client.disconnect()
 
 
 
--- BELLINO_TINNES/Messagerie/serveur.py
+++ BELLINO_TINNES/Messagerie/serveur.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
 import socket
+import sqlite3
+import hashlib
+import msgpack
+from Crypto.Cipher import AES
+from Crypto import Random 
 
 class Serveur:
-    def __init__(self, port):
+    def __init__(self, port, conn, c):
         self.interface = self.obtenir_interface(1, port)
+        self.c = c
+        self.connbd = conn
+        print("Serveur Ok.")
 
     def obtenir_interface(self, temps_attente, port):
         interface = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,15 +25,49 @@ class Serveur:
             conn, addr = self.interface.accept()
             with conn:
                 while True:
+                    isConnected = 0
                     data = conn.recv(1024)
-                    print("getting " + str(data.decode()) + " from " + str(addr))
+                    print(data)
+                    self.c.execute('SELECT KEY, SALT FROM USERS WHERE PSEUDO=?', (data.decode(),))
+                    key = c.fetchone()
+                    print(key)
+                    if not key:
+                        return
+                    msg = "Pseudo correct, entrez le mot de passe : "
+                    conn.send(msg.encode())
+                    mdp = conn.recv(1024)
+                    mymdp = mdp.decode() + key[1]
+                    myhash = hashlib.sha256(mymdp.encode()).hexdigest()
+                    myhash2 = hashlib.sha256(myhash.encode()).hexdigest()
+                    print(myhash2)
+                    print(key[0])
+                    if str(myhash2) == str(key[0]):
+                        self.key = str(myhash)[:32]
+                        msg = "Connection reussi"
+                        conn.send(msg.encode())
+                        nounce = Random.new().read(16) 
+                        self.nounce = nounce
+                        conn.send(nounce)
+                        break
+                    else:
+                        return
+
+                while True:
+                    data = conn.recv(512)
+                    print(self.key)
+                    cipher = AES.new(self.key.encode(), AES.MODE_GCM, self.nounce)
+                    text = cipher.decrypt(data)
+                    text = text.decode('utf-8')
+
+                    print("getting " + text + " from " + str(addr))
+                    conn.send(text.encode())
                 if not data:
                     break
-                conn.sendall(data)
 
 
 if __name__ == "__main__":
-    serveur = Serveur(1234)
+    conn = sqlite3.connect('messagerie.db')
+    c = conn.cursor()
+
+    serveur = Serveur(1234, conn, c)
     serveur.lancement_serveur()
-
-
